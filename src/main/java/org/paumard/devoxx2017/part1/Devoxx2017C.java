2d90874db1dd5fe2e7311ef452cf83c6c9fd09e1
José <Jose.Paumard@gmail.com>
2017-11-12 21:18:04 +0100

    Preparing for Live Coding Session 03

---
+++ src/main/java/org/paumard/devoxx2017/part1/Devoxx2017C.java
@@ -0,0 +1,19 @@
+package org.paumard.devoxx2017.part1;
+
+import org.paumard.devoxx2017.model.Article;
+import org.paumard.devoxx2017.model.Author;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Devoxx2017C {
+
+    public static void main(String[] args) {
+
+        Set<Article> articles = Article.readAll();
+
+    }
+}
