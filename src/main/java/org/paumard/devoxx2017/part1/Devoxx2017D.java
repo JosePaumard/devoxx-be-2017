d74e23759582effcd594e5081d7c9b30ab8bec69
José <Jose.Paumard@gmail.com>
2017-11-14 19:02:33 +0100

    Extracted function and collectors to generic factory methods to improve readability

--- src/main/java/org/paumard/devoxx2017/part1/Devoxx2017D.java
+++ src/main/java/org/paumard/devoxx2017/part1/Devoxx2017D.java
@@ -2,12 +2,15 @@ package org.paumard.devoxx2017.part1;
 
 import org.paumard.devoxx2017.model.Article;
 
+import java.util.Comparator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.collectingAndThen;
 
 public class Devoxx2017D {
 
@@ -16,25 +19,31 @@ public class Devoxx2017D {
         Set<Article> articles = Article.readAll();
 
         // # articles per year
-        Function<Map<Integer, Long>, Entry<Integer, Long>> maxByValue =
-                map -> map.entrySet().stream()
-                        .max(Entry.comparingByValue())
-                        .get();
-
-        Collector<Article, ?, Map<Integer, Long>> groupingBy = Collectors.groupingBy(
-                Article::getInceptionYear,
-                Collectors.counting()
-        );
-
-        Collector<Article, ?, Entry<Integer, Long>> collectingAndThen = Collectors.collectingAndThen(
-                groupingBy, maxByValue
-        );
-
         Entry<Integer, Long> maxNumberOFArticlesPerYear =
                 articles.stream()
-                        .collect(
-                                collectingAndThen
-                        );
+                        .collect(collectingAndThen(
+                                groupingByAndCounting(Article::getInceptionYear), maxByValue()
+                        ));
         System.out.println("maxNumberOFArticlesPerYear = " + maxNumberOFArticlesPerYear);
+    }
+
+    public static <T, K> Collector<T, ?, Map<K, Long>>
+    groupingByAndCounting(Function<T, K> classifier) {
+        return Collectors.groupingBy(
+                classifier,
+                Collectors.counting()
+        );
+    }
+
+    public static <K, V extends Comparable<? super V>> Function<Map<K, V>, Entry<K, V>>
+    maxByValue() {
+        return maxBy(Entry.<K, V>comparingByValue());
+    }
+
+    private static <K, V extends Comparable<? super V>> Function<Map<K, V>, Entry<K, V>>
+    maxBy(Comparator<Entry<K, V>> comparator) {
+        return map -> map.entrySet().stream()
+                .max(comparator)
+                .get();
     }
 }
