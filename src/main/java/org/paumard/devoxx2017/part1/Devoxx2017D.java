865cbb0bf00363c93fa8a3372567889af94a79a0
José <Jose.Paumard@gmail.com>
2017-11-14 18:41:22 +0100

    Starting point of LC 04

--- src/main/java/org/paumard/devoxx2017/part1/Devoxx2017D.java
+++ src/main/java/org/paumard/devoxx2017/part1/Devoxx2017D.java
@@ -1,8 +1,12 @@
 package org.paumard.devoxx2017.part1;
 
 import org.paumard.devoxx2017.model.Article;
+import org.paumard.devoxx2017.util.CollectorsUtils;
 
+import java.util.Comparator;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Devoxx2017D {
 
@@ -10,5 +14,22 @@ public class Devoxx2017D {
 
         Set<Article> articles = Article.readAll();
 
+        // # articles per year
+        Map<Integer, Long> numberOfArticlePerYear =
+                articles.stream()
+                        .collect(
+                                Collectors.groupingBy(
+                                        Article::getInceptionYear,
+                                        Collectors.counting()
+                                )
+                        );
+        System.out.println("numberOfArticlePerYear = " + numberOfArticlePerYear);
+
+        Map.Entry<Integer, Long> maxNumberOFArticlesPerYear =
+                numberOfArticlePerYear
+                        .entrySet().stream()
+                        .max(Comparator.comparing(entry -> entry.getValue()))
+                        .get();
+        System.out.println("maxNumberOFArticlesPerYear = " + maxNumberOFArticlesPerYear);
     }
 }
