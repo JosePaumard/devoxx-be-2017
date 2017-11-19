1d7b1fdc1f85821903f9b7d635899106c3641dc0
José <Jose.Paumard@gmail.com>
2017-11-19 21:35:00 +0100

    Improved the readability of E by methods extractions

--- src/main/java/org/paumard/devoxx2017/part1/Devoxx2017E.java
+++ src/main/java/org/paumard/devoxx2017/part1/Devoxx2017E.java
@@ -6,7 +6,10 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
+
+import static org.paumard.devoxx2017.util.CollectorsUtils.groupingByAndCounting;
 
 public class Devoxx2017E {
 
@@ -18,10 +21,7 @@ public class Devoxx2017E {
         Map<Integer, Long> numberOfArticlePerYear =
                 articles.stream()
                         .collect(
-                                Collectors.groupingBy(
-                                        Article::getInceptionYear,
-                                        Collectors.counting()
-                                )
+                                groupingByAndCounting(Article::getInceptionYear)
                         );
         System.out.println("numberOfArticlePerYear.size() = " + numberOfArticlePerYear.size());
 
@@ -43,23 +43,23 @@ public class Devoxx2017E {
         System.out.println("allMaxesNumberOFArticlesPerYear = " + allMaxesNumberOFArticlesPerYear);
 
         Map.Entry<Long, List<Integer>> allMaxesNumberOFArticlesPerYear2 =
-        numberOfArticlePerYear
-                .entrySet().stream() // Stream<Map.Entry<Integer, Long>>
-                .collect(
-                        Collectors.groupingBy(
-                                entry -> entry.getValue()
+                numberOfArticlePerYear
+                        .entrySet().stream() // Stream<Map.Entry<Integer, Long>>
+                        .collect(
+                                Collectors.groupingBy(
+                                        entry -> entry.getValue()
+                                )
                         )
-                )
-                .entrySet().stream()
-                .collect(
-                        Collectors.toMap(
-                                entry -> entry.getKey(),
-                                entry -> entry.getValue().stream().map(e -> e.getKey()).collect(Collectors.toList())
+                        .entrySet().stream()
+                        .collect(
+                                Collectors.toMap(
+                                        entry -> entry.getKey(),
+                                        entry -> entry.getValue().stream().map(e -> e.getKey()).collect(Collectors.toList())
+                                )
                         )
-                )
-                .entrySet().stream()
-                .max(Comparator.comparing(entry -> entry.getKey()))
-                .get();
+                        .entrySet().stream()
+                        .max(Comparator.comparing(entry -> entry.getKey()))
+                        .get();
         System.out.println("allMaxesNumberOFArticlesPerYear2 bis = " + allMaxesNumberOFArticlesPerYear2);
     }
 }
