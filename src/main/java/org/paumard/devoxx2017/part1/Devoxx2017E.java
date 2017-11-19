e68af6832556661697b9756164958b933fdd5948
José <Jose.Paumard@gmail.com>
2017-11-19 21:00:23 +0100

    Starting point of LC 05

--- src/main/java/org/paumard/devoxx2017/part1/Devoxx2017E.java
+++ src/main/java/org/paumard/devoxx2017/part1/Devoxx2017E.java
@@ -14,5 +14,29 @@ public class Devoxx2017E {
 
         Set<Article> articles = Article.readAll();
 
+        // # articles per year
+        Map<Integer, Long> numberOfArticlePerYear =
+                articles.stream()
+                        .collect(
+                                Collectors.groupingBy(
+                                        Article::getInceptionYear,
+                                        Collectors.counting()
+                                )
+                        );
+        System.out.println("numberOfArticlePerYear.size() = " + numberOfArticlePerYear.size());
+
+        Map.Entry<Long, List<Map.Entry<Integer, Long>>> allMaxesNumberOFArticlesPerYear =
+                numberOfArticlePerYear
+                        .entrySet().stream() // Stream<Map.Entry<Integer, Long>>
+                        .collect(
+                                Collectors.groupingBy(
+                                        entry -> entry.getValue()
+                                )
+                        )
+                        .entrySet().stream()
+                        .max(Comparator.comparing(entry -> entry.getKey()))
+                        .get();
+        System.out.println("allMaxesNumberOFArticlesPerYear = " + allMaxesNumberOFArticlesPerYear);
+
     }
 }
