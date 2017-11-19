055cdb793e7f7ac8580cd0104f29d67fea5b94e8
José <Jose.Paumard@gmail.com>
2017-11-19 21:44:20 +0100

    Third step: writing the collector with the correct type

--- src/main/java/org/paumard/devoxx2017/part2/Devoxx2017F.java
+++ src/main/java/org/paumard/devoxx2017/part2/Devoxx2017F.java
@@ -3,7 +3,9 @@ package org.paumard.devoxx2017.part2;
 import org.paumard.devoxx2017.model.Article;
 
 import java.util.Comparator;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Devoxx2017F {
@@ -14,17 +16,27 @@ public class Devoxx2017F {
 
         // articles with the most authors
         Article articleWithTheMostAuthors =
-        articles.stream()
-                .filter(article -> article.getInceptionYear() > 1900)
-                .max(
-                        Comparator.comparing(article -> article.getAuthors().size())
-                )
-                .get();
+                articles.stream()
+                        .filter(article -> article.getInceptionYear() > 1900)
+                        .max(
+                                Comparator.comparing(article -> article.getAuthors().size())
+                        )
+                        .get();
         System.out.println("articleWithTheMostAuthors = " +
                 articleWithTheMostAuthors.getTitle() + " - " +
                 articleWithTheMostAuthors.getAuthors().size());
 
         // articles with the most authors for each year
+        Collector<Article, ?, Article> collector =
+                Collectors.filtering(
+                        article -> article.getInceptionYear() > 1900,
+                        Collectors.collectingAndThen(
+                                Collectors.maxBy(
+                                        Comparator.comparing(article -> article.getAuthors().size())
+                                ),
+                                Optional::get
+                        )
+                );
 
         articles.stream()
                 .collect(
