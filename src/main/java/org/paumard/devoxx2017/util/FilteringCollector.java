e545b0b78955906b1a5d230772cf60d394839f35
José <Jose.Paumard@gmail.com>
2017-11-19 21:48:27 +0100

    Side example: implementing a nop collector that can become a filtering collector

---
+++ src/main/java/org/paumard/devoxx2017/util/FilteringCollector.java
@@ -0,0 +1,41 @@
+package org.paumard.devoxx2017.util;
+
+import java.util.Set;
+import java.util.function.*;
+import java.util.stream.Collector;
+
+public class FilteringCollector<T, A, R> implements Collector<T, A, R> {
+
+    private Predicate<T> filter;
+    private Collector<T, A, R> downstream;
+
+    public FilteringCollector(Predicate<T> filter, Collector<T, A, R> downstream) {
+        this.filter = filter;
+        this.downstream = downstream;
+    }
+
+    @Override
+    public Supplier<A> supplier() {
+        return downstream.supplier();
+    }
+
+    @Override
+    public BiConsumer<A, T> accumulator() {
+        return downstream.accumulator();
+    }
+
+    @Override
+    public BinaryOperator<A> combiner() {
+        return downstream.combiner();
+    }
+
+    @Override
+    public Function<A, R> finisher() {
+        return downstream.finisher();
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        return downstream.characteristics();
+    }
+}
