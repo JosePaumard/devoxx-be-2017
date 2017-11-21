232024fae3a14af95d7754b7009fcea734002c67
José <Jose.Paumard@gmail.com>
2017-11-21 19:38:36 +0100

    Extracting the most seen duo of authors

--- src/main/java/org/paumard/devoxx2017/part2/Devoxx2017G.java
+++ src/main/java/org/paumard/devoxx2017/part2/Devoxx2017G.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Devoxx2017G {
@@ -17,6 +18,20 @@ public class Devoxx2017G {
     public static void main(String[] args) {
 
         Set<Article> articles = Article.readAll();
+
+        Function<Stream<Author>, Stream<Entry<Author, Author>>> function =
+                authorStream -> StreamsUtils.crossProductOrdered(authorStream, Comparator.comparing(Author::getLastName));
+
+        Map.Entry<Entry<Author, Author>, Long> mostSeenDuo =
+                articles.stream()
+                        .flatMap(article -> function.apply(article.getAuthors().stream()))
+                        .collect(
+                                Collectors.collectingAndThen(
+                                        CollectorsUtils.groupingBySelfAndCounting(),
+                                        CollectorsUtils.maxByValue()
+                                )
+                        );
+        System.out.println("mostSeenDuo = " + mostSeenDuo);
 
     }
 
