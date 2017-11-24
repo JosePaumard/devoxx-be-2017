af6a57ddfb459adcd70082b24ac5d6cda622a64c
José <Jose.Paumard@gmail.com>
2017-11-24 16:44:05 +0100

    Extracting the collector

--- src/main/java/org/paumard/devoxx2017/part2/Devoxx2017I.java
+++ src/main/java/org/paumard/devoxx2017/part2/Devoxx2017I.java
@@ -1,14 +1,8 @@
 package org.paumard.devoxx2017.part2;
 
-import org.paumard.devoxx2017.model.Article;
-import org.paumard.devoxx2017.model.Author;
-import org.paumard.devoxx2017.util.CollectorsUtils;
-
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.function.Function;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class Devoxx2017I {
@@ -18,23 +12,26 @@ public class Devoxx2017I {
         List<String> strings = Arrays.asList("one", "two", "three", "four", "five", "six", "seven");
 
         List<Integer> lengths1 =
-        strings.stream()
-                .map(String::length)
-                .filter(length -> length > 3)
-                .collect(Collectors.toList());
+                strings.stream()
+                        .map(String::length)
+                        .filter(length -> length > 3)
+                        .collect(Collectors.toList());
         System.out.println("lengths1 = " + lengths1);
 
-        List<Integer> lengths2 =
-        strings.stream()
-                .collect(
-                        Collectors.mapping(
-                                String::length,
-                                Collectors.filtering(
-                                        length -> length > 3,
-                                        Collectors.toList()
-                                )
+        Collector<String, ?, List<Integer>> collector =
+                Collectors.mapping(
+                        String::length,
+                        Collectors.filtering(
+                                length -> length > 3,
+                                Collectors.toList()
                         )
                 );
+        
+        List<Integer> lengths2 =
+                strings.stream()
+                        .collect(
+                                collector
+                        );
         System.out.println("lengths2 = " + lengths2);
     }
 }
