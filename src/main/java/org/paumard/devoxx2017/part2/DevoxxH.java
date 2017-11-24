90ebe076f8e127f36b7eb0c535a8024b8bbefd07
José <Jose.Paumard@gmail.com>
2017-11-24 16:01:49 +0100

    Creating a map of articles and list of authors

--- src/main/java/org/paumard/devoxx2017/part2/DevoxxH.java
+++ src/main/java/org/paumard/devoxx2017/part2/DevoxxH.java
@@ -1,8 +1,12 @@
 package org.paumard.devoxx2017.part2;
 
 import org.paumard.devoxx2017.model.Article;
+import org.paumard.devoxx2017.model.Author;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class DevoxxH {
 
@@ -10,6 +14,17 @@ public class DevoxxH {
 
         Set<Article> articles = Article.readAll();
 
+        // Inverting a map
         
+        Map<Article, List<Author>> authorsPerArticles =
+                articles.stream()
+                        .collect(
+                                Collectors.toMap(
+                                        article -> article,
+                                        article -> article.getAuthors()
+                                )
+                        );
+        System.out.println("authorsPerArticles = " + authorsPerArticles.size());
+
     }
 }
