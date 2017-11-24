0c647d84b0bdd682159d66eed29f7a4607552bf5
José <Jose.Paumard@gmail.com>
2017-11-24 16:06:00 +0100

    Inverting the map

--- src/main/java/org/paumard/devoxx2017/part2/DevoxxH.java
+++ src/main/java/org/paumard/devoxx2017/part2/DevoxxH.java
@@ -5,6 +5,7 @@ import org.paumard.devoxx2017.model.Author;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -15,7 +16,7 @@ public class DevoxxH {
         Set<Article> articles = Article.readAll();
 
         // Inverting a map
-        
+
         Map<Article, List<Author>> authorsPerArticles =
                 articles.stream()
                         .collect(
@@ -26,5 +27,16 @@ public class DevoxxH {
                         );
         System.out.println("authorsPerArticles = " + authorsPerArticles.size());
 
+        // Map<Author, List<Article>>
+
+        Map<Author, List<Entry<Article, Author>>> articlesPerAuthor =
+                authorsPerArticles
+                        .entrySet().stream()
+                        .flatMap(entry -> entry.getValue().stream().map(author -> Map.entry(entry.getKey(), author)))
+                        .collect(
+                                Collectors.groupingBy(
+                                        Entry::getValue
+                                )
+                        );
     }
 }
