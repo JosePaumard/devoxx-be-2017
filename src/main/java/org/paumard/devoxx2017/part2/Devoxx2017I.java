f48868fb988cffbf073dba37b175ac82621fd713
José <Jose.Paumard@gmail.com>
2017-11-24 16:42:37 +0100

    Starting point with a stream and a collector pattern

--- src/main/java/org/paumard/devoxx2017/part2/Devoxx2017I.java
+++ src/main/java/org/paumard/devoxx2017/part2/Devoxx2017I.java
@@ -4,6 +4,7 @@ import org.paumard.devoxx2017.model.Article;
 import org.paumard.devoxx2017.model.Author;
 import org.paumard.devoxx2017.util.CollectorsUtils;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -14,7 +15,26 @@ public class Devoxx2017I {
 
     public static void main(String[] args) {
 
-        Set<Article> articles = Article.readAll();
+        List<String> strings = Arrays.asList("one", "two", "three", "four", "five", "six", "seven");
 
+        List<Integer> lengths1 =
+        strings.stream()
+                .map(String::length)
+                .filter(length -> length > 3)
+                .collect(Collectors.toList());
+        System.out.println("lengths1 = " + lengths1);
+
+        List<Integer> lengths2 =
+        strings.stream()
+                .collect(
+                        Collectors.mapping(
+                                String::length,
+                                Collectors.filtering(
+                                        length -> length > 3,
+                                        Collectors.toList()
+                                )
+                        )
+                );
+        System.out.println("lengths2 = " + lengths2);
     }
 }
