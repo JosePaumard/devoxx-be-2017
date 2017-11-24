7b79804046fc18c914c455f1bb2ed16efb4a3204
José <Jose.Paumard@gmail.com>
2017-11-24 15:53:14 +0100

    Preparing for Live Coding session 08

---
+++ src/main/java/org/paumard/devoxx2017/part2/DevoxxH.java
@@ -0,0 +1,15 @@
+package org.paumard.devoxx2017.part2;
+
+import org.paumard.devoxx2017.model.Article;
+
+import java.util.Set;
+
+public class DevoxxH {
+
+    public static void main(String[] args) {
+
+        Set<Article> articles = Article.readAll();
+
+        
+    }
+}
